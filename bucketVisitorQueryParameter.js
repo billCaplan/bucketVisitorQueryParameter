622b4fb773c8b49ad83325a92cd40e040cf177be
Bill Caplan <bill.j.caplan@gmail.com>
2019-08-01 13:05:11 -0700

    Update bucketVisitorQueryParameter.js
    Formatting

--- bucketVisitorQueryParameter.js
+++ bucketVisitorQueryParameter.js
@@ -1,37 +1,36 @@
-function queryCapture(){
+function queryCapture() {
 
-var url = window.location.href; 
+    var url = window.location.href;
 
-if(url.lastIndexOf('&optimizely_id')&& url.lastIndexOf('&optimizely_id')!==-1){
-    secondCall(url,'&optimizely_id');
+    if (url.lastIndexOf('&optimizely_id') && url.lastIndexOf('&optimizely_id') !== -1) {
+        secondCall(url, '&optimizely_id');
+    }
+
+    if (url.lastIndexOf('?optimizely_id') && url.lastIndexOf('?optimizely_id') !== -1) {
+        secondCall(url, '?optimizely_id');
+    }
 }
 
-if(url.lastIndexOf('?optimizely_id')&& url.lastIndexOf('?optimizely_id')!==-1){
-    secondCall(url,'?optimizely_id');
-}
-}
+function secondCall(URL, second) {
 
-function secondCall(URL,second){
+    var target = URL.slice(URL.lastIndexOf(second));
+    var splitIt = target.split('+');
 
-var target = URL.slice(URL.lastIndexOf(second));
-var splitIt = target.split('+');
+    var experimentId = splitIt[0].split('=')[1];
+    var variationId = splitIt[1];
 
-var experimentId = splitIt[0].split('=')[1];
-var variationId = splitIt[1];
-
-window["optimizely"] = window["optimizely"] || [];
-window["optimizely"].push({
-"type": "bucketVisitor",
-"experimentId": experimentId,
-"variationIndex": variationId
-});
+    window["optimizely"] = window["optimizely"] || [];
+    window["optimizely"].push({
+        "type": "bucketVisitor",
+        "experimentId": experimentId,
+        "variationIndex": variationId
+    });
 
 }
-try{
-queryCapture(); 
-}
-catch(error) {
-console.error(error);
-// expected output: ReferenceError: nonExistentFunction is not defined
-// Note - error messages will vary depending on browser
+try {
+    queryCapture();
+} catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
 }
